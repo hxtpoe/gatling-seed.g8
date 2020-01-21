badbb3683bf140077f063254f11fb8d24f06dd52
Łukasz <hxtpoe@users.noreply.github.com>
2020-01-21 13:58:04 +0100

    Fix and add giter8 plugin (#6)
    * fix recursive type and add g8plugin for testing locally
    
    * add giter8.sbt file

--- src/main/g8/src/test/scala/com/acme/ComputerDatabaseApi.scala
+++ src/main/g8/src/test/scala/com/acme/ComputerDatabaseApi.scala
@@ -4,23 +4,6 @@ import scala.concurrent.duration._
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
-class MySimulation extends Simulation with ComputerDatabseApi {
-  val baseUrl = "http://computer-database.gatling.io"
-
-  val httpProtocol = http
-    .baseUrl(baseUrl)
-    .acceptHeader("*/*")
-    .doNotTrackHeader("1")
-    .acceptLanguageHeader("pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7")
-    .acceptEncodingHeader("gzip, deflate, br")
-    .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36")
-
-  val scenario = scenario("Computer database simulation 1")
-    .exec(home)
-    .pause(5)
-
-  setUp(
-    scenario.inject(atOnceUsers(1))
-  ).protocols(httpProtocol)
-
+trait ComputerDatabaseApi extends Simulation {
+  val home = http("home").get("/")
 }
--- src/main/g8/src/test/scala/com/acme/MySimulation.scala
+++ src/main/g8/src/test/scala/com/acme/MySimulation.scala
@@ -4,6 +4,23 @@ import scala.concurrent.duration._
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
-trait ComputerDatabaseApi extends Simulation {
-  val home = http("home").get("/")
+class MySimulation extends Simulation with ComputerDatabaseApi {
+  val baseUrl = "http://computer-database.gatling.io"
+
+  val httpProtocol = http
+    .baseUrl(baseUrl)
+    .acceptHeader("*/*")
+    .doNotTrackHeader("1")
+    .acceptLanguageHeader("pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7")
+    .acceptEncodingHeader("gzip, deflate, br")
+    .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36")
+
+  val computerScn = scenario("Computer database simulation 1")
+    .exec(home)
+    .pause(5)
+
+  setUp(
+    computerScn.inject(atOnceUsers(1))
+  ).protocols(httpProtocol)
+
 }
